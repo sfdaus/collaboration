b38a830e79d084353ce37cc19d66f7e9a5e7e36b
Sukma Firdaus <sfdaus27@yahoo.com>
2025-09-29 20:45:36 +0700

    change response on my thread collabs added application_id

--- transport/response/collaboration_response.go
+++ transport/response/collaboration_response.go
@@ -14,13 +14,13 @@ type ThreadCollaborationApplyRes struct {
 
 // My Thread Collaboration Response
 type MyThreadCollaborationRes struct {
-	ThreadID            string               `json:"thread_id"`
-	ThreadName          string               `json:"thread_name"`
-	ThreadPartnerTypeID string               `json:"thread_partner_type_id"`
-	PartnerTypeName     string               `json:"partner_type_name"`
-	Profile             entity.SimpleProfile `json:"profile"`
-	Status              string               `json:"status"`
-	CreatedAt           int64                `json:"created_at"`
+	ThreadID        string               `json:"thread_id"`
+	ThreadName      string               `json:"thread_name"`
+	ApplicationID   string               `json:"application_id"`
+	PartnerTypeName string               `json:"partner_type_name"`
+	Profile         entity.SimpleProfile `json:"profile"`
+	Status          string               `json:"status"`
+	CreatedAt       int64                `json:"created_at"`
 }
 
 // My Thread Collaboration Requests Response
